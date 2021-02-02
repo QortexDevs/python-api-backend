dab8e70e195b221ea390abb2e398589e6cc71308
Николай Митин <nick.mitin@gmail.com>
2021-02-02 09:58:02 +0300

    Зафиксировали пользователя

--- app/users/models.py
+++ app/users/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import ugettext_lazy as _
-from .managers import CustomUserManager
+
+from .managers import UserManager
 
 
 class User(AbstractUser):
@@ -9,7 +10,8 @@ class User(AbstractUser):
     email = models.EmailField(_('email address'), unique=True)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
-    objects = CustomUserManager()
+
+    objects = UserManager()
 
     def __str__(self):
         return self.email
